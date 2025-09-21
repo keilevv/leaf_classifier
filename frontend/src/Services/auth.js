d6201928b5d812ccadba6cd0f07a4c3987b4b4ec
keilevv <calvihe@gmail.com>
2025-09-20 22:43:44 -0500

    change ports and fix local development script

--- frontend/src/Services/auth.js
+++ frontend/src/Services/auth.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
-console.log("apiUrl", apiUrl);
 axios.defaults.withCredentials = true;
 axios.defaults.headers.common["Content-Type"] = "application/json";
 const authService = {
